79e41118b891c0dcc496f462c364889a7c651535
viliiii <127891594+viliiii@users.noreply.github.com>
2023-04-03 18:13:59 +0200

    2. labos iz OS

---
+++ vjezba_2/Dekker.cpp
@@ -0,0 +1,122 @@
+//
+// Created by vilimsivec on 3/25/23.
+//
+
+#include <stdio.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <cstring>
+#include <string>
+#include <iostream>
+#include <atomic>
+
+int M;
+int Id; /* identifikacijski broj segmenta */
+
+
+typedef struct {
+    int pravo = 0;  /*ako je 1, dijete 1 ima pravo, ako je 2 dijete 2 ima pravo*/
+    std::atomic<int> zastavice[2];
+    int A = 0;
+}spremnik;
+
+void dijete1()
+{
+    spremnik *sprem= (spremnik *) shmat(Id, NULL, 0);
+
+    for(int i=0; i<M; i++){
+        sprem->zastavice[0] = 1;
+       while (sprem->zastavice[1] == 1){
+            if(sprem->pravo != 0){
+                sprem->zastavice[0] = 0;
+                while (sprem->pravo != 0){
+
+                }
+                sprem->zastavice[0] = 1;
+            }
+       }
+
+        sprem->A = sprem->A + 1;
+
+        sprem->pravo = 1;
+        sprem->zastavice[0] = 0;
+    }
+
+    (void) shmdt(sprem);
+}
+
+
+
+void dijete2()
+{
+    spremnik *sprem= (spremnik *) shmat(Id, NULL, 0);
+
+    for(int i=0; i<M; i++){
+        sprem->zastavice[1] = 1;
+        while (sprem->zastavice[0] == 1){
+            if(sprem->pravo != 1){
+                sprem->zastavice[1] = 0;
+                while (sprem->pravo != 1){
+
+                }
+                sprem->zastavice[1] = 1;
+            }
+        }
+        sprem->A = sprem->A + 1;
+        sprem->pravo = 0;
+        sprem->zastavice[1] = 0;
+    }
+
+    (void) shmdt(sprem);
+}
+
+
+
+
+void brisi(int sig)
+{
+    /* oslobađanje zajedničke memorije */
+    (void) shmctl(Id, IPC_RMID, NULL);
+    exit(0);
+}
+int main(int argc, char*argv[], char*env[])
+{
+    M = std::stoi(argv[1]);
+    /* zauzimanje zajedničke memorije */
+    Id = shmget(IPC_PRIVATE, sizeof(spremnik) , 0600);
+
+    if (Id == -1)
+        exit(1);  /* greška - nema zajedničke memorije */
+
+
+    /*spremnik je dio memorije kojemu mmogu pristupati svi procesi kojima se to
+     * dopusti zadavanjem id-a Id*/
+
+
+    /* pokretanje paralelnih procesa */
+
+    if (fork() == 0) {
+        dijete1();
+        exit(0);
+    }
+    if (fork() == 0) {
+        dijete2();
+        exit(0);
+    }
+    (void) wait(NULL);
+    (void) wait(NULL);
+
+    spremnik *sprem= (spremnik *) shmat(Id, NULL, 0);
+    printf("A = %d\n", sprem->A );
+    (void) shmdt(sprem);
+    brisi(0);
+
+
+
+    return 0;
+}
---
+++ vjezba_2/Lamport.cpp
@@ -0,0 +1,81 @@
+//
+// Created by vilimsivec on 3/26/23.
+//
+
+#include <stdio.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <cstring>
+#include <string>
+#include <iostream>
+#include <atomic>
+#include <algorithm>
+#include <csignal>
+
+int N = 0; /*broj dretvi*/
+int M = 0; /*broj ponavljanja za svaku dretvu*/
+int A; /*varijabla koju ce svaka dretva povecavati*/
+std::atomic<int> *ulaz;
+std::atomic<int> *broj;
+
+
+void *lamport(void *x) {
+    int i = *((int *) x);
+
+
+    for (int k = 0; k < M; k++) {
+        ulaz[i] = 1;
+        broj[i] = ((int) *std::max_element(broj, broj + N)) + 1;
+        ulaz[i] = 0;
+
+        for (int j = 0; j < N; j++) {
+            while (ulaz[j] != 0);
+            while (broj[j] != 0 && (broj[j] < broj[i] || (broj[j] == broj[i] && j < i)));
+        }
+        A = A + 1;
+        broj[i] = 0;
+    }
+
+    return NULL;
+}
+
+int main(int argc, char*argv[], char*env[])
+{
+    N = std::stoi(argv[1]);
+    M = std::stoi(argv[2]);
+    ulaz = new std::atomic<int>[N];
+    broj = new std::atomic<int>[N];
+
+    std::fill(ulaz, ulaz + N, 0);
+    std::fill(broj, broj + N, 0);
+
+
+    std::cout<<"N = "<<N<<", M = "<<M<<std::endl;
+
+    pthread_t thr_id[N];
+
+    A = 0;
+    int *br = new int[N]();
+
+    for(int i=0; i<N; i++){
+        br[i] = i;
+    }
+
+    for(int i=0; i<N; i++){
+        /* pokretanje dretvi */
+        if (pthread_create(&thr_id[i], NULL, lamport, &br[i]) != 0) {
+            printf("Greska pri stvaranju dretve!\n");
+            exit(1);
+        }
+    }
+
+    for(int j=0; j<N; j++){
+        /*hvatanje dretvi*/
+        pthread_join(thr_id[j], NULL);
+    }
+
+    std::cout<<"A = "<<A<<std::endl;
+
+    return 0;
+}
